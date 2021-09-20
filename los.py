89c5392a94c81a3570e6d53d2a56b460a6a976dd
texdade <davidetessarolo@gmail.com>
2021-09-20 10:04:16 +0200

    Added /build endpoint, fixed problem with new chromedriver

--- los.py
+++ los.py
@@ -4,13 +4,14 @@ import selenium
 
 from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
+from webdriver_manager.utils import ChromeType
 from selenium.common.exceptions import NoSuchElementException
 from flask import Flask, request
 from flask_restful import Resource, Api
 
 app = Flask(__name__)
 api = Api(app)
-
+'''
 #Install driver
 opts=webdriver.ChromeOptions()
 opts.add_argument('--no-sandbox')
@@ -23,8 +24,8 @@ driver = webdriver.Chrome(options=opts)
 opts=webdriver.ChromeOptions()
 opts.headless=True
 
-driver = webdriver.Chrome(ChromeDriverManager().install() ,options=opts)
-'''
+driver = webdriver.Chrome(ChromeDriverManager(chrome_type=ChromeType.GOOGLE).install() , options=opts)
+
 # GET - returns the best three counters with win rate, empty if no stats are available
 class FindCounter(Resource):
     def get(self, champion):
@@ -130,6 +131,74 @@ class Role(Resource):
             return data, 200
         return data, 404
 
+# GET - returns the graphic icons (64x64) of the items to build for a given champion
+# divided in starter items, main items and boots
+class Build(Resource):
+    def get(self, champion):
+        url = "https://www.op.gg/champion/{}/statistics/".format(champion.lower())
+        driver.get(url)
+        data = {}
+
+        if(driver.page_source != None):
+            starter_items = []
+
+            # scrape starter items first
+            try:
+                ths = driver.find_elements_by_xpath('//th[@class="champion-overview__sub-header"]')
+            except NoSuchElementException:
+                return data, 500
+            
+            for th in ths:
+                if(th.get_attribute('textContent') == "Starter Items"):
+                    items = th.find_elements_by_xpath('./following-sibling::td/ul/li')
+                    
+                    # the icons we are looking for have arrow icons in between them
+                    items = items[::2]
+
+                    for item in items:
+                        starter_items.append(item.find_elements_by_xpath('./img')[0].get_attribute('src'))
+
+            data["starter"] = starter_items
+            
+            #after that, get main build
+            try:
+                ths = driver.find_elements_by_xpath('//th[@class="champion-overview__sub-header"]')
+            except NoSuchElementException:
+                return data, 500
+
+            main_build = []
+
+            for th in ths:
+                if(th.get_attribute('textContent') == "Recommended Build"):  
+                    items = th.find_elements_by_xpath('./following-sibling::td/ul/li')  
+                    # the icons we are looking for have arrow icons in between them
+                    items = items[::2]
+
+                    for item in items:
+                        main_build.append(item.find_elements_by_xpath('./img')[0].get_attribute('src'))
+
+            data["main"] =  main_build
+            
+            #boots last
+            try:
+                ths = driver.find_elements_by_xpath('//th[@class="champion-overview__sub-header"]')
+            except NoSuchElementException:
+                return data, 500
+
+            boots = []
+
+            for th in ths:
+                if(th.get_attribute('textContent') == "Boots"):  
+                    items = th.find_elements_by_xpath('./following-sibling::td/ul/li')  
+                    # the icons we are looking for have arrow icons in between them
+                    items = items[::2]
+
+                    for item in items:
+                        boots.append(item.find_elements_by_xpath('./img')[0].get_attribute('src'))
+
+            data["boots"] =  boots
+            return data, 200
+        return data, 404
 
 class Alive(Resource):
     def get(self):
@@ -139,6 +208,7 @@ class Alive(Resource):
 api.add_resource(FindCounter, '/counters/<string:champion>')
 api.add_resource(WinRates, '/winrates/<string:champion>')
 api.add_resource(Role, '/roles/<string:role>')
+api.add_resource(Build, '/build/<string:champion>')
 api.add_resource(Alive, '/')
 
 if __name__ == '__main__':
